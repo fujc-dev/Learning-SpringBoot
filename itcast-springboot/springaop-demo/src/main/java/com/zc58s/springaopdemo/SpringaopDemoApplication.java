0201aaf9beb2737fd4a99e5d1d4db5ccf5dd0611
fujc <fjc.dane@gmail.com>
2020-12-25 13:56:48 +0800

    SpringBoot - 约定编程 - Spring AOP

---
+++ itcast-springboot/springaop-demo/src/main/java/com/zc58s/springaopdemo/SpringaopDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zc58s.springaopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaopDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaopDemoApplication.class, args);
+    }
+
+}
---
+++ itcast-springboot/springaop-demo/src/test/java/com/zc58s/springaopdemo/SpringaopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zc58s.springaopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaopDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
