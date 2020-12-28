3ba3b78ae21bcb64a7cc238325dc4dae3eb9ef8a
fujc <fjc.dane@gmail.com>
2020-12-28 17:51:35 +0800

    SpringBoot - SpringAOP

---
+++ itcast-springboot/springaop-demo/src/main/java/com/zc58s/springaopdemo/pojo/User.java
@@ -0,0 +1,11 @@
+package com.zc58s.springaopdemo.pojo;
+
+/*
+ * springaop-demo
+ * com.zc58s.springaopdemo.pojo
+ *
+ * @author      : fjc.dane@gmail.com
+ * @create time : 2020/12/28 17:51
+ */
+public class User {
+}
---
+++ itcast-springboot/springaop-demo/src/main/java/com/zc58s/springaopdemo/service/UserService.java
@@ -0,0 +1,15 @@
+package com.zc58s.springaopdemo.service;
+
+import com.zc58s.springaopdemo.pojo.User;
+
+/*
+ * springaop-demo
+ * com.zc58s.springaopdemo.service
+ *
+ * @author      : fjc.dane@gmail.com
+ * @create time : 2020/12/28 17:50
+ */
+public interface UserService {
+    void printUser(User user);
+
+}
