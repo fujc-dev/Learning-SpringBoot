a1e127512f308c8d4d85eabb0bdfebafa9fd0da8
fujc <fjc.dane@gmail.com>
2020-12-29 17:23:08 +0800

    SpringBoot + MyBatis整合

---
+++ itcast-springboot/springboot-mybatis/src/main/java/com/zc58s/springbootmybatis/SpringbootMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.zc58s.springbootmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisApplication.class, args);
+    }
+
+}
