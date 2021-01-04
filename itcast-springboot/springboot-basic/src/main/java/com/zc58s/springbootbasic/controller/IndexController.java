1b95bbda9a9e1306b2f39918a86c4ff48df5f588
fujc <fjc.dane@gmail.com>
2021-01-04 17:02:27 +0800

    SpringBoot - JSP

--- itcast-springboot/springboot-basic/src/main/java/com/zc58s/springbootbasic/controller/IndexController.java
+++ itcast-springboot/springboot-basic/src/main/java/com/zc58s/springbootbasic/controller/IndexController.java
@@ -8,14 +8,17 @@ package com.zc58s.springbootbasic.controller;
  * @create time : 2020/12/23 17:32
  */
 
+import com.zc58s.springbootbasic.pojo.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
 @Controller
+@RequestMapping("/home")
 public class IndexController {
 
     /**
@@ -28,6 +31,24 @@ public class IndexController {
         System.out.println("1");
         model.addAttribute("message", "第一个基于SpringBoot2.3.4构建的JSP项目！");
         return "index";
+    }
+
+    /**
+     *
+     * @return
+     */
+    @RequestMapping("/details")
+    public ModelAndView details() {
+        User user = new User();
+        user.setUsername("Lucy");
+        user.setId(1L);
+        user.setNode("英语老好了！");
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("details");
+        mv.addObject(user);
+        //mv.addObject("aaa",user);
+        return mv;
+
     }
 
 }
