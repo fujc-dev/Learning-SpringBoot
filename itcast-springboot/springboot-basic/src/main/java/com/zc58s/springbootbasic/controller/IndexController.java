0de2f669fe62cef94c5a162b6c9e34adf17386c3
fujc <fjc.dane@gmail.com>
2021-01-04 17:31:51 +0800

    SpringBoot - JSP

--- itcast-springboot/springboot-basic/src/main/java/com/zc58s/springbootbasic/controller/IndexController.java
+++ itcast-springboot/springboot-basic/src/main/java/com/zc58s/springbootbasic/controller/IndexController.java
@@ -9,13 +9,12 @@ package com.zc58s.springbootbasic.controller;
  */
 
 import com.zc58s.springbootbasic.pojo.User;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
-import java.util.Map;
 
 @Controller
 @RequestMapping("/home")
@@ -23,17 +22,19 @@ public class IndexController {
 
     /**
      * 默认页面
+     *
      * @param model
      * @return
      */
     @RequestMapping("/")
-    public String index(Model model){
+    public String index(Model model) {
         System.out.println("1");
         model.addAttribute("message", "第一个基于SpringBoot2.3.4构建的JSP项目！");
         return "index";
     }
 
     /**
+     * SpringMvc使用JSP视图
      *
      * @return
      */
@@ -44,9 +45,28 @@ public class IndexController {
         user.setId(1L);
         user.setNode("英语老好了！");
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("details");
+        mv.setViewName("web/details");  //视图模型，格式\类型不一样
         mv.addObject(user);
         //mv.addObject("aaa",user);
+        return mv;
+
+    }
+
+    /**
+     * SpringMvc使用JSON视图
+     *
+     * @return
+     */
+    @RequestMapping("/json")
+    public ModelAndView json() {
+        User user = new User();
+        user.setUsername("Lucy");
+        user.setId(1L);
+        user.setNode("英语老好了！");
+        ModelAndView mv = new ModelAndView();
+        MappingJackson2JsonView jsonView = new MappingJackson2JsonView();
+        mv.setView(jsonView);  //视图模型，格式\类型不一样
+        mv.addObject(user);
         return mv;
 
     }
