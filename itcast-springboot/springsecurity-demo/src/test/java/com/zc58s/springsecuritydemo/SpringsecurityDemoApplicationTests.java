d6d486090a19ab9806479810abd537ec241467c1
fujc <fjc.dane@gmail.com>
2021-01-07 13:34:08 +0800

    SpringBoot  - Spring Security

---
+++ itcast-springboot/springsecurity-demo/src/test/java/com/zc58s/springsecuritydemo/SpringsecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zc58s.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
