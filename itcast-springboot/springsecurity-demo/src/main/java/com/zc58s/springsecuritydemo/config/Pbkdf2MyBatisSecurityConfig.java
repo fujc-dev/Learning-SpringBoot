bd9a52e3e426dc8b9f772578f79c2f136d4e883f
fujc <fjc.dane@gmail.com>
2021-01-08 10:19:04 +0800

    SpringBoot  - Spring Security - 集成自定义登录界面 +  Amaze UI Login Page

--- itcast-springboot/springsecurity-demo/src/main/java/com/zc58s/springsecuritydemo/config/Pbkdf2MyBatisSecurityConfig.java
+++ itcast-springboot/springsecurity-demo/src/main/java/com/zc58s/springsecuritydemo/config/Pbkdf2MyBatisSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -42,8 +43,17 @@ public class Pbkdf2MyBatisSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        //web.ignoring().antMatchers("/static/js/**", "/static/css/**", "/static/images/**");
+        web.ignoring().antMatchers("/static/assets/js/**", "/static/assets/css/**", "/static/assets/img/**", "/static/assets/fonts/**");
+        // super.configure(web);
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/assets/js/**", "/assets/css/**", "/assets/img/**", "/assets/fonts/**")
+                .permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
