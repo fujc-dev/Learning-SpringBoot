93a36edfde3219792162941b3ab53396dfe0610e
fujc <fjc.dane@gmail.com>
2021-01-11 10:59:56 +0800

    SpringBoot - application.properties

---
+++ itcast-springboot/springboot-base/src/test/java/com/zc58s/springbootbasic/SpringbootBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zc58s.springbootbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
