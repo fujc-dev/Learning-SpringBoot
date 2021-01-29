2825b74f51b3edf3d56c1b13ede1a5923c1deb00
fujc <fjc.dane@gmail.com>
2021-01-29 16:01:18 +0800

    《Java并发编程的艺术》学习笔记 - 一个简单的数据库连接池示例，这解释无敌了，太特么恶习了，这个信号量，通知

--- multit-hreading/Multithreading/src/com/zc58s/chapterone/database/ConnectionPool.java
+++ multit-hreading/Multithreading/src/com/zc58s/chapterone/database/ConnectionPool.java
@@ -9,6 +9,7 @@ import java.util.LinkedList;
  * @createtime : 2021/1/29 14:58
  */
 public class ConnectionPool {
+
     private LinkedList<Connection> pool = new LinkedList<>();
 
 
@@ -20,26 +21,46 @@ public class ConnectionPool {
         }
     }
 
+    /**
+     * 释放连接
+     *
+     * @param connection
+     */
     public void releaseConnection(Connection connection) {
         if (connection != null) {
             synchronized (pool) {
+                //当有线程锁住pool时，等待，让调用了pool.wait时，将连接池添加进去，并执行  pool.notifyAll()通知
+                //然后，当线程释放锁之后，等待获取连接池的线程，就可用继续获取锁，并执行后续下一步
                 pool.addLast(connection);
                 pool.notifyAll();
             }
         }
     }
 
+    /**
+     * 取出连接
+     *
+     * @param mills 时间，在这个时间段内获取连接
+     * @return
+     * @throws InterruptedException
+     */
     public Connection fetchConnection(long mills) throws InterruptedException {
-        synchronized (pool) {
-            if (mills <= 0) {
+        synchronized (pool) {// 锁住了我们的pool对象，在多线程模式下，我们的连接对象，排队取
+            if (mills <= 0) {  //假如，我们传递是时间为小于等于0，那么
                 while (pool.isEmpty()) {
+                    //假如，某一个线程来取连接的时候，连接池里面空了。那么这个线程，就在这里等待，此时线程的状态是WAITING；
+                    //只有，pool调用了notify、notifyAll方法，才会被修改状态为BLOCKED，阻塞，等待别的线程释放锁，然后继续下一步；
+                    //下一步，继续循环呗，如果有连接了，跳出循环，返回连接。否则，继续等待，继续等待别的线程调用notify、notifyAll方法；
                     pool.wait();
                 }
                 return pool.removeFirst();
-            } else {
-                long future = System.currentTimeMillis() + mills;
-                long remaining = mills;
-                while (pool.isEmpty() && remaining > 0) {
+            } else { //有设置超时间的，就是我们的mills>0
+                long future = System.currentTimeMillis() + mills;  //我们将结束时间计算出来
+                long remaining = mills; //此时，remaining肯定是有值的
+                while (pool.isEmpty() && remaining > 0) {  //remaining有值，且连接池里面没有有连接时，逻辑跟上面的部分差不多
+                    //唯一，有区别的是，有一个超时时间
+                    //在指定时间内，我们循环的在pool连接池中去取。
+                    //
                     pool.wait(remaining);
                     remaining = future - System.currentTimeMillis();
                 }
--- multit-hreading/Multithreading/src/com/zc58s/chapterone/database/ConnectionPoolTest.java
+++ multit-hreading/Multithreading/src/com/zc58s/chapterone/database/ConnectionPoolTest.java
@@ -5,31 +5,38 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- *
  * @author : fjc.dane@gmail.com
  * @createtime : 2021/1/29 15:11
  */
 public class ConnectionPoolTest {
 
+    //==================================================
+    //初期的准备工作，线程池初始出来
     static ConnectionPool pool = new ConnectionPool(10);
+    //计数器
     static CountDownLatch start = new CountDownLatch(1);
     static CountDownLatch end;
+    //==================================================
 
+    /**
+     * @param args
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws InterruptedException {
         int threadCount = 10;
         end = new CountDownLatch(threadCount);
         int count = 20;
-
+        //AtomicInteger，基于CAS的算法的原子操作，自旋锁
         AtomicInteger got = new AtomicInteger();
         AtomicInteger notGot = new AtomicInteger();
+        //开启10个线程去获取连接
         for (int i = 0; i < threadCount; i++) {
-            Thread thread = new Thread(new ConnectionRunner(count, got, notGot),
-                    "ConnectionRunnerThread");
+            Thread thread = new Thread(new ConnectionRunner(count, got, notGot), "ConnectionRunnerThread");
             thread.start();
         }
-
-        start.countDown();
-        end.await();
+        start.countDown(); //将count值减1。这是控制start.await()运行的，直到count减到为0时，start.await()后面的代码才会执行。
+        System.out.println(threadCount);  //此时是0，
+        end.await();  //此时也是同理，等待end里面的count为0时，后面的代码才会被执行。
         System.out.println("total invoke: " + (threadCount * count));
         System.out.println("got connection: " + got);
         System.out.println("not got connection " + notGot);
@@ -48,10 +55,13 @@ public class ConnectionPoolTest {
 
         public void run() {
             try {
-                start.await();
+                start.await();  //调用await()方法的线程会被挂起，它会等待直到count值为0才继续执行
+                //我们声明的时候，将start的count设置为1，然后，在main方法中，调用了一个countDown，那此时
+                //我们的start的count就是为0，那么会执行下一步
+                System.out.println("1111");
             } catch (Exception ex) {
-            }
 
+            }
             while (count > 0) {
                 try {
                     // 从线程池中获取连接，如果1000ms内无法获取到，将会返回null
@@ -61,6 +71,8 @@ public class ConnectionPoolTest {
                         try {
                             connection.createStatement();
                             connection.commit();
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
                         } finally {
                             pool.releaseConnection(connection);
                             got.incrementAndGet();
