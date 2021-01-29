f7a1341d6f522d593f9173436556c906081e9f40
fujc <fjc.dane@gmail.com>
2021-01-29 13:37:42 +0800

    《Java并发编程的艺术》学习笔记 -- 线程中断，如果遇到Sleep阻塞，会出现异常，推荐使用cancel

---
+++ multit-hreading/Multithreading/src/com/zc58s/chapterone/Interrupted.java
@@ -0,0 +1,54 @@
+package com.zc58s.chapterone;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 中断线程。
+ *
+ * <p>
+ * Interrupted与Sleep的关系？
+ * </p>
+ *
+ * @author : fjc.dane@gmail.com
+ * @createtime : 2021/1/29 12:52
+ */
+public class Interrupted {
+
+    public static void main(String[] args) throws InterruptedException {
+// sleepThread不停的尝试睡眠
+        Thread sleepThread = new Thread(new SleepRunner(), "SleepThread");
+        sleepThread.setDaemon(true);
+        // busyThread不停的运行
+        Thread busyThread = new Thread(new BusyRunner(), "BusyThread");
+        busyThread.setDaemon(true);
+        sleepThread.start();
+        busyThread.start();
+        // 休眠5秒，让sleepThread和busyThread充分运行
+        TimeUnit.SECONDS.sleep(5);
+        sleepThread.interrupt();
+        busyThread.interrupt();
+        System.out.println("SleepThread interrupted is " + sleepThread.isInterrupted());
+        System.out.println("BusyThread interrupted is " + busyThread.isInterrupted());
+        // 防止sleepThread和busyThread立刻退出
+        //
+        TimeUnit.SECONDS.sleep(2);
+    }
+
+    static class SleepRunner implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                SleepUtils.second(10);
+            }
+        }
+    }
+
+    static class BusyRunner implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+
+            }
+        }
+    }
+}
