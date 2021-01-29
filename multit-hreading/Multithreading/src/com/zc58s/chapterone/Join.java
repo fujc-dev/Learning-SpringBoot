c756037ca0fe59c730da2dfead8c93409ad2601c
fujc <fjc.dane@gmail.com>
2021-01-29 14:38:28 +0800

    《Java并发编程的艺术》学习笔记 - Join

---
+++ multit-hreading/Multithreading/src/com/zc58s/chapterone/Join.java
@@ -0,0 +1,44 @@
+package com.zc58s.chapterone;
+
+/**
+ * join，字面意思就是等待，等待其他线程执行完毕，再回来执行当前线程。
+ *
+ * @author : fjc.dane@gmail.com
+ * @createtime : 2021/1/29 14:22
+ */
+public class Join {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread previous = Thread.currentThread();
+        for (int i = 0; i < 10; i++) {
+            //创建了一个线程。在线程内调用线程的join方法，阻塞当前线程，等待其他线程执行完毕
+            Thread thread = new Thread(new Domino(previous), String.valueOf(i));
+            //thread.setDaemon(true);
+            thread.start();
+            previous = thread;
+        }
+        System.out.println("Main thread is finished");
+    }
+
+
+    static class Domino implements Runnable {
+
+        private Thread thread;
+
+        public Domino(Thread thread) {
+            this.thread = thread;
+        }
+
+        @Override
+        public void run() {
+            try {
+                this.thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(Thread.currentThread().getName() + " terminate .");
+            }
+        }
+    }
+}
