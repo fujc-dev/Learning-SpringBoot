544f678533bade5ad3f84dceeddb8574ede77122
fujc <fjc.dane@gmail.com>
2021-01-29 17:03:12 +0800

    《Java并发编程的艺术》学习笔记 - 这本书真垃圾，读起来，真费劲。

---
+++ multit-hreading/Multithreading/src/com/zc58s/chapterone/locked/LockUseCase.java
@@ -0,0 +1,19 @@
+package com.zc58s.chapterone.locked;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author : fjc.dane@gmail.com
+ * @createtime : 2021/1/29 16:22
+ */
+public class LockUseCase {
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+        lock.lock();
+        try {
+        } finally {
+            lock.unlock();
+        }
+    }
+}
