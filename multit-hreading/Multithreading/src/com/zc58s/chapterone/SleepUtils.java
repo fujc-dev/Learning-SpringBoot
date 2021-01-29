6f587be27584cce39489892ef0acfb115db2366d
fujc <fjc.dane@gmail.com>
2021-01-29 13:39:10 +0800

    《Java并发编程的艺术》学习笔记 -- 工具

---
+++ multit-hreading/Multithreading/src/com/zc58s/chapterone/SleepUtils.java
@@ -0,0 +1,17 @@
+package com.zc58s.chapterone;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author : fjc.dane@gmail.com
+ * @createtime : 2021/1/29 12:56
+ */
+public class SleepUtils {
+    public static void second(int second) {
+        try {
+            TimeUnit.SECONDS.sleep(second);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
