3c808edcb3ab08c2939e808298cefb36a5d47e64
fujc <fjc.dane@gmail.com>
2021-02-01 17:08:50 +0800

    SpringBoot RabbitMQ应用

---
+++ springboot-rabbitmq/src/test/java/com/zc58s/springbootrabbitmq/SpringbootRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zc58s.springbootrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
