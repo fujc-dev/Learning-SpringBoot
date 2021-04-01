9fd683a7b49d86326ef1f840aaddd65727170c2b
fujc <fjc.dane@gmail.com>
2021-04-01 14:39:02 +0800

    基于modbus4j库实现的Modbus通信

--- modbus4j-gateway/src/main/java/com/kggs/modbus4jgateway/strategy/service/impl/ReadCoilsStrategy.java
+++ modbus4j-gateway/src/main/java/com/kggs/modbus4jgateway/strategy/service/impl/ReadCoilsStrategy.java
@@ -22,10 +22,32 @@ public class ReadCoilsStrategy extends StrategyBase {
     @Override
     public void Read(int slaveId, List<SlavePoint> points) {
         if (points != null) {
+            //一个设备的所有点位
+            //存储的时候，按照offset绑定的点位，依次存储到数据库
+
+            boolean[] vals = ReadSlaveByAllPoints(slaveId, points);
+            for (boolean val : vals) {
+                System.out.println("Value：" + val);
+            }
             for (SlavePoint point : points) {
                 Boolean val = this.readCoilStatus(slaveId, point.getOffset());
                 System.out.println("Offset：" + point.getOffset() + "Value：" + val);
             }
         }
+    }
+
+    /**
+     * 一次性读取设备上所有点位的开关量
+     *
+     * @param slaveId
+     * @param points
+     * @return
+     */
+    private boolean[] ReadSlaveByAllPoints(int slaveId, List<SlavePoint> points) {
+        boolean[] vals = new boolean[points.size()];
+        for (int i = 0; i < points.size(); i++) {
+            vals[i] = this.readCoilStatus(slaveId, points.get(i).getOffset());
+        }
+        return vals;
     }
 }
